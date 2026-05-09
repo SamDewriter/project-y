3b7a4c3d5000477f24cfeda8168aaa46ab3a0726
Mubarak <dewriter.sam@gmail.com>
2026-05-09 18:41:33 +0100

    Revert "Add index.py with basic print statements"
    This reverts commit c180cf8322a686391354ea1e16ded3a1c469bd8d.
    
    revert

--- index.py
+++
@@ -1,7 +0,0 @@
-print("This is a Git repository.")
-print("We are learning Git and GitHub.")
-
-# Stage the changes
-# git add index.py
-# Commit the changes
-# git commit -m "Add index.py with basic print statements"
